5ed44fd08af166787014c6e75b86cc839e277a93
Rodrigo Silva <rodrigo.7762018@gmail.com>
2025-03-22 18:22:53 -0300

    adicionando funçoes de buscar selecao, put e delete etc

--- src/app.js
+++ src/app.js
@@ -7,11 +7,20 @@ app.use(express.json())
 
 //mock
 const selecoes = [
-    {id: 1, selecao: 'Brasil', group: 'G'},
-    {id: 2, selecao: 'Suíça', group: 'G'},
-    {id: 3, selecao: 'Sérvia', group: 'G'},
-    {id: 4, selecao: 'Camarões', group: 'G'}
+    {id: 1, selecao: 'Brasil', grupo: 'G'},
+    {id: 2, selecao: 'Suíça', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'}
 ]
+
+function buscarSelecaoPorId(id){
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+//Pegar a posicao do elemento no array por id
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
 
 // criando rota padrão
 app.get('/', (req, res) => {
@@ -22,9 +31,29 @@ app.get('/selecoes', (req, res) =>{
     res.send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    let index = req.params.id
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) =>{
     selecoes.push(req.body)
     res.status(201).send('Seleçao cadastrada com suscesso!')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    console.log(index)
+    selecoes.splice(index, 1)
+    res.send(`Seleçao com id ${req.params.id} excluida com suscesso!`)
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    console.log(index)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo   = req.body.grupo
+    res.json(selecoes)
 })
 
 export default app
